f81b8e45aaaa726fe60768ee41b310f5dd13cce9
KyeBuff <kyebuffery14@gmail.com>
2020-02-17 16:31:45 +0000

    Added CPT archives

--- site/web/app/themes/sage/app/cpt.php
+++ site/web/app/themes/sage/app/cpt.php
@@ -13,11 +13,16 @@ Class CPT {
           'singular_name' => ['Event'],
 
         ],
+        'public'             => true,
+        'publicly_queryable' => true,
         'description' => 'Add Events here',
         'show_ui' => true,
         'show_in_menu' => true,
-        'show_nav_menus' => true,
-        'menu_icon' => 'dashicons-calendar-alt'
+        'show_in_nav_menus' => true,
+        'show_in_admin_bar' => true,
+        'menu_icon' => 'dashicons-calendar-alt',
+        'has_archive' => true,
+        'rewrite' => ['slug' => 'events']
       ]
     );
   }
@@ -31,11 +36,15 @@ Class CPT {
           'singular_name' => ['Image'],
 
         ],
+        'public'             => true,
+        'publicly_queryable' => true,
         'description' => 'Add Images here',
         'show_ui' => true,
         'show_in_menu' => true,
-        'show_nav_menus' => true,
-        'menu_icon' => 'dashicons-camera-alt'
+        'show_in_nav_menus' => true,
+        'show_in_admin_bar' => true,
+        'menu_icon' => 'dashicons-camera-alt',
+        'has_archive' => true,
       ]
     );
   }
@@ -49,11 +58,15 @@ Class CPT {
           'singular_name' => ['resource'],
 
         ],
+        'public'             => true,
+        'publicly_queryable' => true,
         'description' => 'Add Resources here',
         'show_ui' => true,
         'show_in_menu' => true,
-        'show_nav_menus' => true,
-        'menu_icon' => 'dashicons-calendar-alt'
+        'show_in_nav_menus' => true,
+        'show_in_admin_bar' => true,
+        'menu_icon' => 'dashicons-calendar-alt',
+        'has_archive' => true,
       ]
     );
   }
