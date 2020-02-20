bd9ac523336ba57ffd2ae3ad37ce973e5b62e38c
Kye Buffery <kye@Olis-MBP.home>
2020-02-20 16:36:55 +0000

    Mobile header toggling

--- site/web/app/themes/sage/resources/assets/scripts/routes/common.js
+++ site/web/app/themes/sage/resources/assets/scripts/routes/common.js
@@ -1,8 +1,15 @@
 export default {
-  init() {
-    // JavaScript to be fired on all pages
-  },
-  finalize() {
-    // JavaScript to be fired on all pages, after page specific JS is fired
-  },
+	init() {
+		// JavaScript to be fired on all pages
+		$('#nav-toggle').on('click', (e) => {
+			const nav = $('.nav-primary');
+			const isOpen = nav.attr('data-toggle') === 'open';
+
+			$(e.target).text(isOpen ? 'X' : 'Menu');
+			nav.attr('data-toggle', isOpen ? 'close' : 'open');
+		});
+	},
+	finalize() {
+		// JavaScript to be fired on all pages, after page specific JS is fired
+	},
 };
--- site/web/app/themes/sage/resources/views/partials/header.blade.php
+++ site/web/app/themes/sage/resources/views/partials/header.blade.php
@@ -1,7 +1,8 @@
 <header class="banner">
-  <div class="container">
+  <div class="container header-container">
     <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
+    <button id="nav-toggle">Menu</button>
+    <nav class="nav-primary" data-toggle="close">
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif
