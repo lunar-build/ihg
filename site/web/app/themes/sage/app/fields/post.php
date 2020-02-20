7fb0a5334c85b886d5a050e93c7f9e94313de93d
Kye Buffery <kye@Olis-MBP.home>
2020-02-20 15:15:11 +0000

    Added Post Type

--- site/web/app/themes/sage/app/fields/post.php
+++ site/web/app/themes/sage/app/fields/post.php
@@ -9,4 +9,23 @@ $post = new FieldsBuilder('post');
 $post
     ->setLocation('post_type', '==', 'post');
 
-    return $post;
+$post 
+    ->addSelect('select_field', [
+        'label' => 'Post Type',
+        'instructions' => '',
+        'required' => 1,
+        'wrapper' => [
+            'width' => '',
+            'class' => '',
+            'id' => '',
+        ],
+        'choices' => ['Latest News', 'In the Media'],
+        'default_value' => 'Latest News',
+        'allow_null' => 0,
+        'multiple' => 0,
+        'ui' => 0,
+        'ajax' => 0,
+        'return_format' => 'value',
+        'placeholder' => '',  
+    ]);
+return $post;
