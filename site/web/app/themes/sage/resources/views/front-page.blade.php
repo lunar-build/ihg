0728e71b59ae7c02a77d0cc669b8138fbf8946b4
Kye Buffery <kye@Olis-MBP.home>
2020-02-21 11:05:42 +0000

    Fixed in media bug

--- site/web/app/themes/sage/resources/views/front-page.blade.php
+++ site/web/app/themes/sage/resources/views/front-page.blade.php
@@ -27,9 +27,9 @@
 	@php $in_media = FrontPage::get_in_media(); @endphp
 	@while ($in_media->have_posts()) @php $in_media->the_post(); $media = new Archive; @endphp
 		{{-- @todo - create partial  --}}
-		{{ $post->title() }}
-		{{ $post->post_date() }}
-		@php echo $post->description() @endphp
+		{{ $media->title() }}
+		{{ $media->post_date() }}
+		@php echo $media->description() @endphp
 	@endwhile
 
 @endsection
