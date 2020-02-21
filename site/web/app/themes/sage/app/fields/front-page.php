cbd233a52bf1d906ad4213c2ee5a7fed4565ba9a
Kye Buffery <kye@Olis-MBP.home>
2020-02-21 11:29:22 +0000

    Added front page tiles

---
+++ site/web/app/themes/sage/app/fields/front-page.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App;
+
+use StoutLogic\AcfBuilder\FieldsBuilder;
+
+$page = new FieldsBuilder('page');
+
+$page
+    ->setLocation('post_type', '==', 'page')
+    ->and('page_type', '==', 'front_page');
+
+$group =
+    $page->addGroup('hero_tiles', [
+        'label' => 'Hero Tiles',
+        'instructions' => 'This content will appear in the tiles found within the hero at the top of the front page',
+        'required' => 1,
+        'layout' => 'table',
+    ]);
+
+$group 
+    ->addGroup('tile_1', [
+        'label' => 'Tile 1',
+        'instructions' => '',
+        'required' => 1,
+        'layout' => 'inline',
+    ])
+        ->addText('Heading')
+        ->addTextArea('Text');
+
+$group 
+    ->addGroup('tile_2', [
+        'label' => 'Tile 2',
+        'instructions' => '',
+        'required' => 1,
+        'layout' => 'inline',
+    ])
+        ->addText('Heading')
+        ->addTextArea('Text');
+
+$group 
+    ->addGroup('tile_3', [
+        'label' => 'Tile 3',
+        'instructions' => '',
+        'required' => 1,
+        'layout' => 'inline',
+    ])
+        ->addText('Heading')
+        ->addTextArea('Text');
+
+return $page;
