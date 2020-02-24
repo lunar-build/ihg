eb81983e9c989c8095b2a5e09f3383ff5784ca85
Charlotte <ckdrew670@hotmail.com>
2020-02-24 12:38:28 +0000

    Add markup to footer

--- site/web/app/themes/sage/resources/views/partials/footer.blade.php
+++ site/web/app/themes/sage/resources/views/partials/footer.blade.php
@@ -1,4 +1,13 @@
 <footer class="main-footer content-info">
-  <div class="container">
-  </div>
+    <div class="container footer-container">
+        <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+        <nav class="nav-footer">
+            <ul class="nav-footer-list">
+                <li class="nav-footer-list-item"><a href="/">{{ get_bloginfo('name', 'display') }}</a></li>
+                <li class="nav-footer-list-item"><a href="/">Privacy Policy</a></li>
+                <li class="nav-footer-list-item"><a href="/">Terms & Conditions</a></li>
+                <li class="nav-footer-list-item"><a href="/">Contact Us</a></li>
+            </ul>
+        </nav>
+    </div>
 </footer>
