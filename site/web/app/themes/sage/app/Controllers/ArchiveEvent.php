e003e3650592102dd2b487a3623a769fc4c9635f
Charlotte <ckdrew670@hotmail.com>
2020-02-25 12:42:39 +0000

    Changed date format

--- site/web/app/themes/sage/app/Controllers/ArchiveEvent.php
+++ site/web/app/themes/sage/app/Controllers/ArchiveEvent.php
@@ -26,7 +26,7 @@ class ArchiveEvent extends Controller
     public function date()
     {
         $date = date_create(get_field('date'));
-        return date_format($date, 'Md');
+        return date_format($date, 'dM');
     } 
 
     public function link()
