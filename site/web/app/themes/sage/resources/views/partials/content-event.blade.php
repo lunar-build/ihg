b4c3c6f644b60fa17819fce9e796410c9e3fd5fb
Charlotte <ckdrew670@hotmail.com>
2020-02-25 12:29:11 +0000

    Markup for events content. Added some styling, switching branch to retrieve brand colors.

--- site/web/app/themes/sage/resources/views/partials/content-event.blade.php
+++ site/web/app/themes/sage/resources/views/partials/content-event.blade.php
@@ -1,4 +1,8 @@
-{{ $event->title() }}
-{{ $event->description() }}
-{{ $event->date() }}
-{{ $event->link() }}
+<div class="event-card">
+    <div class="event-date-title">
+        <p>{{ $event->date() }}</p>
+        <h3>{{ $event->title() }}</h3>
+    </div>
+    <p>{{ $event->description() }}<p>
+    <a href="{{ $event->link() }}">View event</a>
+</div>
