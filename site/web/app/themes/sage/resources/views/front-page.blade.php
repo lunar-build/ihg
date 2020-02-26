2dc553c3e913a2911256c740e624330adbfaa506
Charlotte <ckdrew670@hotmail.com>
2020-02-26 16:07:21 +0000

    Add styling to front page panels. Also resize press-cards and hr therein

--- site/web/app/themes/sage/resources/views/front-page.blade.php
+++ site/web/app/themes/sage/resources/views/front-page.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-	<section class="front-page-panel container-inner">
+	<section class="container-inner">
 		<div class="news-panel">
 			@php $news = FrontPage::get_news(3); @endphp
 			@while ($news->have_posts()) @php $news->the_post(); $post = new Archive @endphp
@@ -10,7 +10,7 @@
 		</div>
 	</section>
 	
-	<section class="front-page-panel container-inner">
+	<section class="container-inner">
 		<div class="events-panel">
 			@php $events = FrontPage::get_events(); @endphp
 			@while ($events->have_posts()) @php $events->the_post(); $event = new ArchiveEvent; @endphp
@@ -19,7 +19,7 @@
 		</div>
 	</section>
 
-	<section class="front-page-panel container-inner">
+	<section class="container-inner">
 		<div class="image-gallery-panel">
 			@php $images = FrontPage::get_image_gallery(); @endphp
 			@while ($images->have_posts()) @php $images->the_post(); $image = new ArchiveImage; @endphp
@@ -28,7 +28,7 @@
 		</div>
 	</section>
 
-	<section class="front-page-panel container-inner">
+	<section class="container-inner">
 		<div class="resources-panel">
 			@php $resources = FrontPage::get_resources(); @endphp
 			@while ($resources->have_posts()) @php $resources->the_post(); $resource = new ArchiveResource; @endphp
@@ -37,7 +37,7 @@
 		</div>
 	</section>
 
-	<section class="front-page-panel container-inner">
+	<section class="container-inner">
 		<div class="press-panel">
 			@php $in_media = FrontPage::get_in_media(3); @endphp
 			@while ($in_media->have_posts()) @php $in_media->the_post(); $media = new Archive; @endphp
