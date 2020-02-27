92518d1880cc150bcd757c33eeb458b775993024
Charlotte <ckdrew670@hotmail.com>
2020-02-27 14:42:28 +0000

    Add initial styling to hero layout.

--- site/web/app/themes/sage/resources/views/partials/hero-panel.blade.php
+++ site/web/app/themes/sage/resources/views/partials/hero-panel.blade.php
@@ -1,7 +1,7 @@
 <section class="hero-panel">
-    <h1 class="hero_heading">{{ $hero_heading }}</h1>
-    <div class="hero-text">
-        <h2 class="hero-subheading">{{ $hero_subheading }}</h2>
+    <h1 class="hero-heading">{{ strtoupper($hero_heading) }}</h1>
+    <div class="hero-body">
+        <h2 class="hero-subheading">{{ strtoupper($hero_subheading) }}</h2>
         <hr class="hero-rule">
         <p class="hero-text">{{ $hero_text }}</p> 
     </div>
