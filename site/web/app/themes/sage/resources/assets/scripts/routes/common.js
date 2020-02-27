ecb0d5c32102e044260f79642663a329dea9d1e2
Charlotte <ckdrew670@hotmail.com>
2020-02-27 14:51:50 +0000

    Add Kye's JQuery for burger menu

--- site/web/app/themes/sage/resources/assets/scripts/routes/common.js
+++ site/web/app/themes/sage/resources/assets/scripts/routes/common.js
@@ -1,10 +1,27 @@
 export default {
 	init() {
+		
+		// Get/create icons with classes for FA
+		const closeIcon = $('<i />');
+		closeIcon.addClass('fas fa-times');
+		const hamburger = $('.fa-bars');
+
 		// JavaScript to be fired on all pages
 		$('#nav-toggle').on('click', () => {
 			const nav = $('.nav-primary');
 			const isOpen = nav.attr('data-toggle') === 'open';
+			const menuBtn = $('#nav-toggle');
 			nav.attr('data-toggle', isOpen ? 'close' : 'open');
+
+			menuBtn.children().remove(); // Remove child SVG
+
+			// Append FA <i />, which jQuery will then use to render SVG
+			if (isOpen) {
+				menuBtn.append(hamburger);
+			} else {
+				menuBtn.append(closeIcon);
+			}
+			
 		});
 	},
 	finalize() {
