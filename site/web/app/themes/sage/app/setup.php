5e270c206ddd6b232dc98e341e922fb2c4c5b074
Kye Buffery <kye@Olis-MacBook-Pro.local>
2020-02-28 14:49:34 +0000

    setup vars first

--- site/web/app/themes/sage/app/setup.php
+++ site/web/app/themes/sage/app/setup.php
@@ -18,8 +18,8 @@ add_action('wp_enqueue_scripts', function () {
     preg_match('/[a-z]+.([a-z-]*).([a-z]+)/', $full_path, $matches);
     $brand_stylesheet = $matches[1];
 
-    wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
     wp_enqueue_style('sage/'.$brand_stylesheet.'.css', asset_path('styles/'.$brand_stylesheet.'.css'), false, null);
+    wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
     wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
 
     if (is_single() && comments_open() && get_option('thread_comments')) {
