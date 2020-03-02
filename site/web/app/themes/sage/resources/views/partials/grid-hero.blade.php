19476eb8263605c80b4b0ecbbccfab13e4a0e289
Kye Buffery <kye@Olis-MBP.home>
2020-03-02 09:55:11 +0000

    Colour tweak

--- site/web/app/themes/sage/resources/views/partials/grid-hero.blade.php
+++ site/web/app/themes/sage/resources/views/partials/grid-hero.blade.php
@@ -2,7 +2,7 @@
     <div class="news-card-text">
         <header>
             <p class="news-card-date"><time>{{ $post->formatted_date() }}</time></p>
-            <h1 class="entry-title">{!! get_the_title() !!}</h1>
+            <h2 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
         </header>
         <div class="entry-content">
             @php the_content() @endphp
