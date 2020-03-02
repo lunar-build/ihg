2c0ef790d65a97b75fcbec00004a2f9e73aeb6c1
Kye Buffery <kye@Olis-MacBook-Pro-2.local>
2020-03-02 13:00:26 +0000

    Added default filter

--- site/web/app/themes/sage/resources/views/partials/post-filters.blade.php
+++ site/web/app/themes/sage/resources/views/partials/post-filters.blade.php
@@ -1,5 +1,5 @@
 <select id="{{$filter_type}}-filter" class="post_filter" name="{{$filter_type}}">
-    <option value="*">All</option>
+    <option value="{{str_replace(home_url(), '', $default_value)}}">All</option>
     @foreach ($filters as $f)
 <option value="{{str_replace(home_url(), '', get_term_link($f->term_id))}}">{{html_entity_decode($f->name)}}</option>
     @endforeach
