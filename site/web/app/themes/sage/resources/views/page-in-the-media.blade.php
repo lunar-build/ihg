d7d647973f34983899682c943663292fb053b298
Kye Buffery <kye@Olis-MacBook-Pro-2.local>
2020-03-02 12:12:46 +0000

    Added grid to media

--- site/web/app/themes/sage/resources/views/page-in-the-media.blade.php
+++ site/web/app/themes/sage/resources/views/page-in-the-media.blade.php
@@ -1,11 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
-  @php $in_media = FrontPage::get_in_media(); @endphp
-  @component('layouts.article-grid')
-    @while ($in_media->have_posts()) @php $in_media->the_post(); $post = new Archive; @endphp
-  			@include('partials.content-news')
-    @endwhile
-  @endcomponent
+	@include('partials.page-header')
+	@php $news = FrontPage::get_news(); @endphp
+	@component('layouts.article-grid')
+		@php $count = 0 @endphp
+		@while ($news->have_posts()) @php $news->the_post(); $post = new Archive @endphp
+			@if(empty($count))
+				@include('partials.grid-hero')
+			@else
+				@include('partials.content-news')
+			@endif
+			@php $count++ @endphp
+		@endwhile
+	@endcomponent
 @endsection
