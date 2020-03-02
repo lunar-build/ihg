51cf045aaf698e4abac1da0e8e2626f8325f5616
Charlotte <ckdrew670@hotmail.com>
2020-03-02 09:27:40 +0000

    Styling for hero panel

--- site/web/app/themes/sage/resources/views/partials/hero-panel.blade.php
+++ site/web/app/themes/sage/resources/views/partials/hero-panel.blade.php
@@ -1,4 +1,9 @@
 <section class="hero-panel">
+    {{--IMAGE  --}}
+    <figure class="hero-image">
+        {!! App::create_responsive_image($hero_image) !!}
+    </figure>
+    
     <h1 class="hero-heading">{{ strtoupper($hero_heading) }}</h1>
     <div class="hero-body">
         <h2 class="hero-subheading">{{ strtoupper($hero_subheading) }}</h2>
@@ -6,7 +11,3 @@
         <p class="hero-text">{{ $hero_text }}</p> 
     </div>
 </section>
-
-{{--IMAGE  --}}
-{{-- {!! App::create_responsive_image($hero_image) !!} --}}
-{!! App::get_image_url($hero_image) !!}
