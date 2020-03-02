09d3332b2e1ae0302d857086aa88b3f934132dca
Kye Buffery <kye@Olis-MacBook-Pro-2.local>
2020-03-02 15:40:11 +0000

    started article single

---
+++ site/web/app/themes/sage/resources/views/partials/content-single-post.blade.php
@@ -0,0 +1,19 @@
+@php $post = new ArchivePost; @endphp
+<article @php post_class(); $category = get_the_category()[0]; @endphp>
+  <header>
+    <div class="single-post-header">
+      <div class="meta">
+        <p class="archive-title">{{$category->name}}</p>
+        <p class="date"><time>{{ $post->formatted_date() }}</time></p>
+        <h1 class="entry-title">{!! get_the_title() !!}</h1>
+      </div>
+      <figure class="image">
+        {!! App::create_responsive_image($post->get_image()) !!}
+      </figure>
+    </div>
+  </header>
+  <div class="entry-content">
+  </div>
+  <footer>
+  </footer>
+</article>
