fdfb504159c348f4a43c5fa00cbca64c1d7ee652
Kye Buffery <kye@Olis-MBP-2.home>
2020-03-03 15:30:03 +0000

    Hero desktop working

--- site/web/app/themes/sage/resources/views/partials/hero-panel.blade.php
+++ site/web/app/themes/sage/resources/views/partials/hero-panel.blade.php
@@ -4,7 +4,9 @@
         {!! App::create_responsive_image($hero_image) !!}
     </figure>
     
-    <h1 class="hero-heading">{{ strtoupper($hero_heading) }}</h1>
+    <header class="hero-heading">
+        <h1>{{ strtoupper($hero_heading) }}</h1>
+    </header>
     <div class="hero-body">
         <h2 class="hero-subheading">{{ strtoupper($hero_subheading) }}</h2>
         <p class="hero-text">{{ $hero_text }}</p> 
