de9d6911a6e5636998b90991d67003e9e63b0327
Kye Buffery <kye@Olis-MBP-2.home>
2020-03-03 11:19:52 +0000

    Hero mobile nearly there

--- site/web/app/themes/sage/resources/views/partials/hero-panel.blade.php
+++ site/web/app/themes/sage/resources/views/partials/hero-panel.blade.php
@@ -7,7 +7,6 @@
     <h1 class="hero-heading">{{ strtoupper($hero_heading) }}</h1>
     <div class="hero-body">
         <h2 class="hero-subheading">{{ strtoupper($hero_subheading) }}</h2>
-        <hr class="hero-rule">
         <p class="hero-text">{{ $hero_text }}</p> 
     </div>
 </section>
