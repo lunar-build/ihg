80d2dd440ff96af75e104725c0f45512b5c376ed
Kye Buffery <kye@Olis-MBP-2.home>
2020-03-03 10:33:43 +0000

    Styled modal desktop

--- site/web/app/themes/sage/resources/views/partials/modal.blade.php
+++ site/web/app/themes/sage/resources/views/partials/modal.blade.php
@@ -4,11 +4,15 @@
 <div class="modal modal--hide">
     <div class="modal-inner">
         <button class="modal-close js-modal-close btn-default">X</button>
-        {!! App::create_responsive_image($attachment_id) !!}
-        <section class="modal-content">
-            <h3 class="modal-title">{{$title}}</h3>
-            <p class="modal-meta">Some image size</p>
-        </section>
-        <a class="modal-cta" download href="{{wp_get_attachment_url($attachment_id)}}">Download</a>
+        <figure class="featured-image">
+            {!! App::create_responsive_image($attachment_id) !!}
+        </figure>
+        <div class="modal-side">
+            <section class="modal-content">
+                <h3 class="modal-title">{{$title}}</h3>
+                <p class="modal-meta">Some image size</p>
+            </section>
+            <a class="modal-cta" download href="{{wp_get_attachment_url($attachment_id)}}">Download</a>
+        </div>
     </div>
 </div>
