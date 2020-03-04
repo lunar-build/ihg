16263120393e955b5ff9d7f7d74163735f0eefb2
Kye Buffery <kye@Olis-MBP-2.home>
2020-03-04 17:26:04 +0000

    Finished media styling

--- site/web/app/themes/sage/resources/views/partials/content-press.blade.php
+++ site/web/app/themes/sage/resources/views/partials/content-press.blade.php
@@ -1,21 +1,16 @@
 <article class="press-card" >
-    <div {!! post_class() !!}>
-        {{-- DATE --}}
-        <hr class="separator-top">
-        <p class="press-card-date spaced-text">{{ $post->formatted_date() }}</p>
-
-        {{-- TITLE --}}
-        <h2 class="entry-title press-title">
-            <a href="{{ get_permalink() }}">{!! get_the_title() !!}</a>
-        </h2>
-        <hr class="separator-bottom">
-        <a class="press-card-link" href="{{ get_permalink() }}">
-            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25">
-                <g fill="none" fill-rule="evenodd" stroke="#CCC" stroke-linecap="square" stroke-width="3" transform="rotate(-45 14.54 7.98)">
-                    <line x1="14" x2="14" y1="14"/>
-                    <line x1="14" y1="14" y2="14"/>
-                </g>
-            </svg>
-        </a>
-    </div>
+    {{-- DATE --}}
+    <p class="press-card-date spaced-text">{{ $post->formatted_date() }}</p>
+    {{-- TITLE --}}
+    <h2 class="entry-title press-title">
+        <a href="{{ get_permalink() }}">{!! get_the_title() !!}</a>
+    </h2>
+    <a class="press-card-link" href="{{ get_permalink() }}">
+        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25">
+            <g fill="none" fill-rule="evenodd" stroke="#CCC" stroke-linecap="square" stroke-width="3" transform="rotate(-45 14.54 7.98)">
+                <line x1="14" x2="14" y1="14"/>
+                <line x1="14" y1="14" y2="14"/>
+            </g>
+        </svg>
+    </a>
 </article>
