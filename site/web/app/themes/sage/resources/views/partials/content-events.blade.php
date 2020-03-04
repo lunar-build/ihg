a55551a06e8957bdfcb61f9b715b0fba562a1533
Kye Buffery <kye@Olis-MBP-2.home>
2020-03-04 11:15:46 +0000

    Added map link and styling

--- site/web/app/themes/sage/resources/views/partials/content-events.blade.php
+++ site/web/app/themes/sage/resources/views/partials/content-events.blade.php
@@ -9,18 +9,19 @@
     </header>
     <div class="content">
         <section class="event-info">
-            <p class="event-datetime">
+            <div class="event-meta">
                 <p><strong>Date & Time</strong></p>
                 <time>{{$post->long_date()}}</time>
                 <time>{{$post->start_time()}} - {{$post->end_time()}}</time>
-                Doors open: {{$post->doors_open()}}
-            </p>
-            <p class="event-location">
+                <p>Doors open: {{$post->doors_open()}}</p>
+            </div>
+            <div class="event-meta">
                 <p><strong>Location</strong></p>
                 @foreach ($post->address() as $a)
                     <p>{{$a['Line']}}</p>
                 @endforeach
-            </p>
+                <a class="btn-default link-primary" ref="#">View map</a>
+            </div>
         </section>
         <section>
             @include('partials.youtube-embed', [
