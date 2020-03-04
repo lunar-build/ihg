04a43fe622755e9fd2dc7ed08a2c96b0932d7367
Kye Buffery <kye@Olis-MBP-2.home>
2020-03-04 13:13:49 +0000

    Added logo

--- site/web/app/themes/sage/resources/views/partials/header.blade.php
+++ site/web/app/themes/sage/resources/views/partials/header.blade.php
@@ -5,7 +5,13 @@
       <img class="logo--white" src="{{App::get_image_asset('logo_white.png')}}" alt="Logo" />
     </a>
     <div id="nav-toggle">
-      <object width="100%" data="{{App::get_image_asset('menu.svg')}}"></object>
+      <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48">
+        <g fill="none" fill-rule="evenodd" stroke="#A00062" stroke-linecap="square" stroke-width="2" transform="translate(10 16)">
+          <line x1=".246" x2="27.754" y1=".5" y2=".5"/>
+          <line x1=".246" x2="27.754" y1="8.5" y2="8.5"/>
+          <line x1=".246" x2="27.754" y1="16.5" y2="16.5"/>
+        </g>
+      </svg>
     </div>
     <nav class="nav-primary" data-toggle="close">
       @if (has_nav_menu('primary_navigation'))
