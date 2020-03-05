8be9ab044d8a6ede3a3094f8c92b5c76dfc46783
Kye Buffery <kye@Olis-MBP-2.home>
2020-03-05 15:40:26 +0000

    header singlr working

--- site/web/app/themes/sage/resources/views/partials/content-post.blade.php
+++ site/web/app/themes/sage/resources/views/partials/content-post.blade.php
@@ -2,7 +2,7 @@
 @php $category = get_the_category()[0]; @endphp
 <article class="single-post">
   <header>
-    <div class="single-post-header">
+    <div class="header">
       <div class="meta">
         <p class="archive-title spaced-text">{{$category->name}}</p>
         <p class="date spaced-text"><time>{{ $post->formatted_date() }}</time></p>
@@ -17,10 +17,10 @@
     @php echo $post->description() @endphp
   </div>
   <footer class="single-post-footer">
-    <h3>Other News</h3>
+    <h3 class="spaced-text uppercase">Other News</h3>
     @component('layouts.article-grid')
       @php $related_posts = $post->get_related(); @endphp
-      @while ($related_posts->have_posts(2)) @php $related_posts->the_post(); $post = new ArchivePost @endphp
+      @while ($related_posts->have_posts()) @php $related_posts->the_post(); $post = new ArchivePost @endphp
         @include('partials.content-news')
       @endwhile
     @endcomponent
