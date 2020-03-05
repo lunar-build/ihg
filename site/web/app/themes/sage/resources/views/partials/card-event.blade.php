fa0d6c356dcad55b54c607486f0c699119bef6c0
Kye Buffery <kye@Olis-MBP-2.home>
2020-03-05 14:15:46 +0000

    Added cursor pointer

--- site/web/app/themes/sage/resources/views/partials/card-event.blade.php
+++ site/web/app/themes/sage/resources/views/partials/card-event.blade.php
@@ -3,7 +3,7 @@
         <div class="event-date">
             <p>{{ $event->date() }}</p>
         </div>
-        <h3>{{ $event->title() }}</h3>
+        <h3><a href="{{ $event->link() }}">{{ $event->title() }}</a></h3>
     </header>
     
     <p>@php echo strlen($event->description()) < 123 ? $event->description() : substr($event->description(), 0, 123) . '...' @endphp<p>
