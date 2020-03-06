795a582bcb6b356434c843f83f4c820c6425726f
Kye Buffery <kye@Olis-MBP-2.home>
2020-03-06 15:12:30 +0000

    Refactored press coverage

--- site/web/app/themes/sage/resources/views/page-press-coverage.blade.php
+++ site/web/app/themes/sage/resources/views/page-press-coverage.blade.php
@@ -2,15 +2,9 @@
 @section('content')
 	@include('partials.page-header')
 	@component('layouts.article-grid')
-        @php $count = 0 @endphp
 		@php $media = FrontPage::get_in_media(); @endphp
 		@while ($media->have_posts()) @php $media->the_post(); $post = new ArchivePost @endphp
-			@if(empty($count))
-				@include('partials.grid-hero')
-			@else
-				@include('partials.content-news')
-			@endif
-			@php $count++ @endphp
+			@include('partials.grid-hero')
         @endwhile
         @include('partials.pagination', [
             'result' => $media
--- site/web/app/themes/sage/resources/views/partials/page-header.blade.php
+++ site/web/app/themes/sage/resources/views/partials/page-header.blade.php
@@ -1,6 +1,6 @@
 <div class="page-header">
   @if (get_post_type() != 'events')
-    <h1>{!! App::title() !!}</h1>
+    <h1 class="spaced-text uppercase">{!! App::title() !!}</h1>
   @endif
   @if (!empty($filters))
   	@include('partials.post-filters')
