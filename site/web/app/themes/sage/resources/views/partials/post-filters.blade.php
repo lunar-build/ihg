16e81246b52c87af9b3baeeb49d411ea92fa337a
Kye Buffery <kye@Olis-MBP-2.home>
2020-03-09 13:49:49 +0000

    Added ALL tag

--- site/web/app/themes/sage/resources/views/partials/post-filters.blade.php
+++ site/web/app/themes/sage/resources/views/partials/post-filters.blade.php
@@ -10,14 +10,21 @@
         <p data-selected="selected" data-value="{{str_replace(home_url(), '', $default['value'])}}">Category: {{ html_entity_decode($default['label'])}}</p>
         <div class="modal modal--hide">
             <div class="filter-options">
+                @php
+                    $current_id = get_queried_object()->term_id;
+                @endphp
                 <p 
-                    class="filter-option"
-                    data-selected="selected" data-value="{{str_replace(home_url(), '', $default['value'])}}"
-                >Category: {{ html_entity_decode($default['label'])}}</p>
+                    class="filter-option" 
+                    data-value="/whats-new"
+                    data-selected="{{empty($current_id) ? 'selected' : ''}}"
+                >
+                    All
+                </p>
                 @foreach ($filters as $f)
                     <p 
                         class="filter-option" 
                         data-value="{{str_replace(home_url(), '', get_term_link($f->term_id))}}"
+                        data-selected="{{$current_id == $f->term_id ? 'selected' : ''}}"
                     >
                     {{html_entity_decode($f->name)}}</p>
                 @endforeach
