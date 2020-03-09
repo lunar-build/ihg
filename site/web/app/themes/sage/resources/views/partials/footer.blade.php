1c2b8a1168225b5ba7025cb08b201ffb57c85abe
Kye Buffery <kye@Olis-MBP-2.home>
2020-03-09 16:56:08 +0000

    Footer from menu

--- site/web/app/themes/sage/resources/views/partials/footer.blade.php
+++ site/web/app/themes/sage/resources/views/partials/footer.blade.php
@@ -5,10 +5,9 @@
         </a>
         <nav class="nav-footer">
             <ul class="nav-footer-list">
-                <li class="nav-footer-list-item"><a href="/">{{ get_bloginfo('name', 'display') }}</a></li>
-                <li class="nav-footer-list-item"><a href="/">Privacy Policy</a></li>
-                <li class="nav-footer-list-item"><a href="/">Terms & Conditions</a></li>
-                <li class="nav-footer-list-item"><a href="/">Contact Us</a></li>
+                @foreach (wp_get_nav_menu_items('footer-menu') as $item)
+                    <li class="nav-footer-list-item"><a href="{{$item->url}}">{{ $item->post_title }}</a></li>
+                @endforeach
             </ul>
         </nav>
     </div>
