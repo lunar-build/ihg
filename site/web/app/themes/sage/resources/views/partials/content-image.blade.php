7abeddd816f7fb8fa2cebdb7cab17cdc469b459d
Kye Buffery <kye@Olis-MBP-2.home>
2020-03-11 14:39:53 +0000

    New single post layout

--- site/web/app/themes/sage/resources/views/partials/content-image.blade.php
+++ site/web/app/themes/sage/resources/views/partials/content-image.blade.php
@@ -1,17 +1,5 @@
-<article class="preview-card gallery-card has-hover">
     {{-- FEATURED IMAGE --}}
     <figure class="featured-image">
-        @component('partials.modal', [
-            'title' => $image->title(),
-            'text' => get_the_content(),
-            'attachment_id' => $image->get_image(),
-            'file' => [
-                'type' => $image->get_file_type(),
-                'size' => $image->get_file_size()
-            ]
-        ])
-            {!! App::create_responsive_image($image->get_image()) !!}
-        @endcomponent
+        {!! App::create_responsive_image($image->get_image()) !!}
     </figure>
-</article>
 
